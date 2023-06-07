70120ec2812c23e3dca346d17969e280251fbc30
vinay <vinayvinnu0990@gmail.com>
2023-06-08 00:35:24 +0530

    UI updated

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const bodyParser = require
+("body-parser");
 const app = express();
+app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
-
+let location = "";
+app.get("/",(req,res)=>{
+    res.render("index",{link : location});
+});
 app.post("/location",(req,res)=>{
-    console.log(res.body);
+    location = req.body.location;
+    res.redirect("/");
 });
 
 const port = 3000 || process.env.PORT;
